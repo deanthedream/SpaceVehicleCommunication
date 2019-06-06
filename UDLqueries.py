6975031fe4479120082fcc6f746e25ec6d45813b
Dean Keithly <drk94@cornell.edu>
2019-06-06 12:43:12 -0600

    functionify word units to astropy units

--- UDLqueries.py
+++ UDLqueries.py
@@ -20,33 +20,18 @@ arr_allSensors = dat_allSensors[col_allSensors]#.values # extracts data as an ar
 #Query Help All Sensors
 url_help = "https://unifieddatalibrary.com/udl/sensor/queryhelp"
 dat_help, col_help = g.queryUDL(url_help)
+dat_help['parameters'] = g.addAstropyUnits_to_queryHelp(dat_help)
 
-params = dat_help['parameters']
 
-#Trying to convert these into manipulatable quantities in astropy
-#{'units of magnitude', 'Hz', None, 'degrees', 'km/sec', 'rad/min', 'Watts', 'decibels', 'kilometers', 'radians'}
-allUnits = set([param['unitOfMeasure'] for param in params])
-for i in np.arange(len(params)):
-    if params[i]['unitOfMeasure'] == None:
-        continue
-    elif params[i]['unitOfMeasure'] == 'kilometers':
-        params[i]['unit'] = u.kilometer
-    elif params[i]['unitOfMeasure'] == 'km/sec':
-        params[i]['unit'] = u.kilometer/u.second
-    elif params[i]['unitOfMeasure'] == 'Hz':
-        params[i]['unit'] = 1./u.second
-    elif params[i]['unitOfMeasure'] == 'degrees':
-        params[i]['unit'] = u.degree
-    elif params[i]['unitOfMeasure'] == 'rad/min':
-        params[i]['unit'] = u.radian/u.minute
-    elif params[i]['unitOfMeasure'] == 'Watts':
-        params[i]['unit'] = u.joule/u.second
-    elif params[i]['unitOfMeasure'] == 'radians':
-        params[i]['unit'] = u.radian
-    elif params[i]['unitOfMeasure'] == 'units of magnitude':
-        params[i]['unit'] = u.mag
-    elif params[i]['unitOfMeasure'] == 'decibels':
-        params[i]['unit'] = u.decibel
-    else:
-        print('unhandled unit: ' + str(params[i]['unitOfMeasure']))
+
+#query observation help
+url_observationhelp = "https://unifieddatalibrary.com/udl/observation/queryhelp"
+dat_observationhelp, col_observationhelp = g.queryUDL(url_observationhelp)
+dat_observationhelp['parameters'] = g.addAstropyUnits_to_queryHelp(dat_observationhelp)
+
+#query rfobservation help
+url_rfobservationhelp = "https://unifieddatalibrary.com/udl/rfobservation/queryhelp"
+dat_rfobservationhelp, col_rfobservationhelp = g.queryUDL(url_rfobservationhelp)
+dat_rfobservationhelp['parameters'] = g.addAstropyUnits_to_queryHelp(dat_rfobservationhelp)
+
 
--- grabUDL.py
+++ grabUDL.py
@@ -5,6 +5,8 @@ and unpacking all data
 #call by "import grabUDL.grabUDL as grabUDL" at top of script
 
 import requests, base64, pandas as pd
+import astropy.units as u
+import numpy as np
 
 
 class grabUDL:
@@ -34,3 +36,43 @@ class grabUDL:
         #elsetsDataFrame.keys() #Gets all possible columns
         #keepColumns = ['epoch','meanMotion','eccentricity','inclination','meanAnomaly']
         #elsetsArray = elsetsDataFrame[keepColumns].values
+
+    def addAstropyUnits_to_queryHelp(self, queryhelp):
+        """Trying to convert these into manipulatable quantities in astropy
+        {'units of magnitude', 'Hz', None, 'degrees', 'km/sec', 'rad/min', 'Watts', 'decibels', 'kilometers', 'radians'}
+        """
+        params = queryhelp['parameters']
+        allUnits = set([param['unitOfMeasure'] for param in params])
+        for i in np.arange(len(params)):
+            if params[i]['unitOfMeasure'] == None:
+                continue
+            elif params[i]['unitOfMeasure'] == 'kilometers' or \
+                 params[i]['unitOfMeasure'] == 'km':
+                params[i]['unit'] = u.kilometer
+            elif params[i]['unitOfMeasure'] == 'km/sec':
+                params[i]['unit'] = u.kilometer/u.second
+            elif params[i]['unitOfMeasure'] == 'Hz':
+                params[i]['unit'] = 1./u.second
+            elif params[i]['unitOfMeasure'] == 'degrees':
+                params[i]['unit'] = u.degree
+            elif params[i]['unitOfMeasure'] == 'rad/min':
+                params[i]['unit'] = u.radian/u.minute
+            elif params[i]['unitOfMeasure'] == 'Watts':
+                params[i]['unit'] = u.joule/u.second
+            elif params[i]['unitOfMeasure'] == 'radians':
+                params[i]['unit'] = u.radian
+            elif params[i]['unitOfMeasure'] == 'units of magnitude':
+                params[i]['unit'] = u.mag
+            elif params[i]['unitOfMeasure'] == 'decibels':
+                params[i]['unit'] = u.decibel
+            elif params[i]['unitOfMeasure'] == 'degrees per second':
+                params[i]['unit'] = u.degree/u.second
+            elif params[i]['unitOfMeasure'] == 'seconds':
+                params[i]['unit'] = u.second
+            elif params[i]['unitOfMeasure'] == 'dBW':
+                params[i]['unit'] = u.decibel*u.joule/u.second
+            elif params[i]['unitOfMeasure'] == 'dBW/Hz':
+                params[i]['unit'] = u.decibel*u.joule
+            else:
+                print('unhandled unit: ' + str(params[i]['unitOfMeasure']))
+        return params
